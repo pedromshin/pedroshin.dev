e02ff2723c33b4f1067ae032ae9a3d3538c98805
Pedro Shin <pedro.shin@afya.com.br>
2022-03-12 22:52:57 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"]
+};

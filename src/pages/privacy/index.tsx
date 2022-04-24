0077eaa2017c4bad57f8e6fe5cc791ccb4eca070
Pedro Shin <pedro.shin@afya.com.br>
2022-04-24 18:43:46 -0300

    creates privacy page

---
+++ src/pages/privacy/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function IndexPage() {
+  return (
+    <main>
+      <div>
+        <p>Página de privacidade</p>
+      </div>
+    </main>
+  );
+}
+
+export default IndexPage;

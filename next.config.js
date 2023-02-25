cc6f2d62a4c2db0230a05249f952e6a3d885195c
Pedro Shin <pedro.shin@flashapp.com.br>
2023-02-25 18:28:36 -0300

    fix next config settings to deploy on amplify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

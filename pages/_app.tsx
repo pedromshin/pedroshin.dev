256053d075e30126d893b3d6d189e8aed6d95634
Pedro Shin <pedro.shin@flashapp.com.br>
2023-02-28 21:06:31 -0300

    structure to old folders and blank pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from 'next/app'
-import '../styles/index.css'
+import type { AppProps } from "next/app";
+import "../styles/index.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,17 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-        </Head>
-        <body className="bg-black antialiased">
+        <Head></Head>
+        <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;

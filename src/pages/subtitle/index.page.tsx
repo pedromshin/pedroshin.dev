46b72c851238b62293119fd7386fd23bf3f6d33a
Pedro Shin <pedromaschio.shin@gmail.com>
2023-07-06 23:58:58 -0300

    implements video to text transcript

--- src/pages/subtitle/index.page.tsx
+++ src/pages/subtitle/index.page.tsx
@@ -1,7 +1,23 @@
 import useWhisper from "./useWhisper";
-import { Group, Stack, Text } from "@mantine/core";
-import { Dropzone, IMAGE_MIME_TYPE, PDF_MIME_TYPE } from "@mantine/dropzone";
+import { Button, Group, Stack, Text } from "@mantine/core";
+import { Dropzone } from "@mantine/dropzone";
+import { RawAxiosRequestHeaders } from "axios";
 import { useState } from "react";
+
+const config = {
+  apiKey: "",
+  autoStart: false,
+  autoTranscribe: true,
+  mode: "transcriptions",
+  nonStop: false,
+  removeSilence: false,
+  streaming: false,
+  timeSlice: 1_000,
+  onDataAvailable: undefined,
+  onTranscribe: undefined,
+};
+
+const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
 
 const Subtitle = () => {
   const {
@@ -13,17 +29,147 @@ const Subtitle = () => {
     startRecording,
     stopRecording,
   } = useWhisper({
-    apiKey: process.env.OPEN_AI_KEY,
+    apiKey: OPEN_AI_KEY,
   });
-
   const [fileName, setFileName] = useState<string>("");
-  const [videoUrl, setVideoUrl] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [audioResult, setAudioResult] = useState<any>();
+  const [videoTranscript, setVideoTranscript] = useState<string>();
+  const [error, setError] = useState<string>();
+
+  function bufferToWave(audioBuffer: AudioBuffer) {
+    var numOfChan = audioBuffer.numberOfChannels;
+    var length = audioBuffer.length * numOfChan * 2 + 44;
+    var buffer = new ArrayBuffer(length);
+    var view = new DataView(buffer);
+    var channels = [],
+      i,
+      sample,
+      offset = 0,
+      pos = 0;
+
+    // Write WAVE header
+    setUint32(0x46464952); // "RIFF"
+    setUint32(length - 8); // file length - 8
+    setUint32(0x45564157); // "WAVE"
+
+    setUint32(0x20746d66); // "fmt " chunk
+    setUint32(16); // length = 16
+    setUint16(1); // PCM (uncompressed)
+    setUint16(numOfChan);
+    setUint32(audioBuffer.sampleRate);
+    setUint32(audioBuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+    setUint16(numOfChan * 2); // block-align
+    setUint16(16); // 16-bit (hardcoded in this demo)
+
+    setUint32(0x61746164); // "data" - chunk
+    setUint32(length - pos - 4); // chunk length
+
+    // Write interleaved data
+    for (i = 0; i < audioBuffer.numberOfChannels; i++)
+      channels.push(audioBuffer.getChannelData(i));
+
+    while (pos < length) {
+      for (i = 0; i < numOfChan; i++) {
+        // interleave channels
+        sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
+        sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767) | 0; // scale to 16-bit signed int
+        view.setInt16(pos, sample, true); // write 16-bit sample
+        pos += 2;
+      }
+      offset++; // next source sample
+    }
+
+    // Helper functions
+    function setUint16(data: any) {
+      view.setUint16(pos, data, true);
+      pos += 2;
+    }
+
+    function setUint32(data: any) {
+      view.setUint32(pos, data, true);
+      pos += 4;
+    }
+
+    // Return the Blob containing the WAV file
+    return new Blob([buffer], { type: "audio/wav" });
+  }
+
+  const sampleRate = 16000;
+  const numberOfChannels = 1;
+  let myBuffer: AudioBuffer;
 
   const loadFile = (file: File) => {
-    setVideoUrl(URL.createObjectURL(file));
     setFileName(file.name);
 
-    console.log(file);
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onload = function () {
+      const audioContext = new window.AudioContext();
+      const videoFileAsBuffer = reader.result;
+      audioContext
+        .decodeAudioData(videoFileAsBuffer as ArrayBuffer)
+        .then(function (decodedAudioData) {
+          const duration = decodedAudioData.duration;
+
+          const offlineAudioContext = new OfflineAudioContext(
+            numberOfChannels,
+            sampleRate * duration,
+            sampleRate
+          );
+          const soundSource = offlineAudioContext.createBufferSource();
+
+          myBuffer = decodedAudioData;
+          soundSource.buffer = myBuffer;
+          soundSource.connect(offlineAudioContext.destination);
+          soundSource.start();
+
+          offlineAudioContext
+            .startRendering()
+            .then(function (renderedBuffer) {
+              console.log(1, renderedBuffer); // outputs audiobuffer
+              const wavFileBlob = bufferToWave(renderedBuffer);
+              setAudioResult(new File([wavFileBlob], "audio.wav"));
+            })
+            .catch(function (err) {
+              console.log("Rendering failed: " + err);
+            });
+        });
+    };
+
+    // reader.readAsArrayBuffer(file);
+    console.log(2, file);
+  };
+
+  const extract = async () => {
+    setLoading(true);
+    const body = new FormData();
+    body.append("file", audioResult);
+    body.append("model", "whisper-1");
+    if (config.mode === "transcriptions") {
+      body.append("language", "pt");
+    }
+
+    const headers: RawAxiosRequestHeaders = {};
+    headers["Content-Type"] = "multipart/form-data";
+    headers["Authorization"] = `Bearer ${OPEN_AI_KEY}`;
+    const { default: axios } = await import("axios");
+    try {
+      const response = await axios.post(
+        "https://api.openai.com/v1/audio/" + config.mode,
+        body,
+        {
+          headers,
+        }
+      );
+
+      setVideoTranscript(response.data.text);
+      return response.data.text;
+    } catch (error) {
+      setError((error as any)?.response?.data?.error.message);
+    }
+    setLoading(false);
   };
 
   return (
@@ -62,6 +208,25 @@ const Subtitle = () => {
               </Text>
             )}
           </Dropzone>
+          <Stack style={{ flex: "1" }}>
+            <Button
+              style={{
+                background: "blue",
+              }}
+              onClick={extract}
+            >
+              {loading ? "Carregando..." : "Extrair"}
+            </Button>
+
+            {!!videoTranscript && (
+              <Stack>
+                <Text size="xl" inline>
+                  {videoTranscript}
+                </Text>
+              </Stack>
+            )}
+            {error}
+          </Stack>
         </Stack>
       </Group>
     </>

7acec174e80683cf7c98db609173a69ee908a856
Pedro Shin <pedromaschio.shin@gmail.com>
2023-07-06 23:12:05 -0300

    implement audio to text

--- src/pages/subtitle/index.page.tsx
+++ src/pages/subtitle/index.page.tsx
@@ -1,8 +1,30 @@
+import useWhisper from "./useWhisper";
+
 const Subtitle = () => {
+  const {
+    recording,
+    speaking,
+    transcript,
+    transcribing,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: process.env.OPEN_AI_KEY,
+  });
+
+  console.log({ recording, speaking, transcribing, transcript });
+
   return (
-    <>
-      <h1>Subtitle</h1>
-    </>
+    <div>
+      <p>Recording: {recording ? "Gravando" : ""}</p>
+      <p>Speaking: {speaking ? "Falando" : ""}</p>
+      <p>Transcripting: {transcribing}</p>
+      <p>Transcribed Text: {transcript.text}</p>
+      <button onClick={() => startRecording()}>Start</button>
+      <button onClick={() => pauseRecording()}>Pause</button>
+      <button onClick={() => stopRecording()}>Stop</button>
+    </div>
   );
 };
 

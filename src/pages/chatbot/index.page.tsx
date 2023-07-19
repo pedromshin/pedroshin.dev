8b1aace9a978b81935ff40aafc3d519c87bdfb9f
Pedro Shin <pedromaschio.shin@gmail.com>
2023-07-19 18:04:58 -0300

    improve style

--- src/pages/chatbot/index.page.tsx
+++ src/pages/chatbot/index.page.tsx
@@ -4,21 +4,51 @@ import React from "react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  const containerStyle = {
+    width: "400px",
+    margin: "0 auto",
+    padding: "20px",
+  };
+
+  const messageStyle = {
+    marginBottom: "10px",
+    padding: "5px",
+    borderRadius: "5px",
+  };
+
+  const roleStyle = {
+    fontWeight: "bold",
+    marginRight: "5px",
+  };
+
+  const formStyle = {
+    marginTop: "20px",
+  };
+
   return (
-    <div>
+    <div style={containerStyle}>
       <div>
         {messages?.map((m) => (
-          <div key={m?.id}>
-            <span>{m?.role}:</span>
+          <div key={m?.id} style={messageStyle}>
+            <span style={roleStyle}>{m?.role}:</span>
             <span>{m?.content}</span>
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} style={formStyle}>
         <input
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
+          style={{
+            padding: "10px",
+            fontSize: "16px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+            width: "100%",
+            outline: "none",
+          }}
         />
       </form>
     </div>

11597268eb2cc311417b1e3a2448227f570138eb
Pedro Shin <pedromaschio.shin@gmail.com>
2023-07-20 19:25:53 -0300

    runs embed

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     OPEN_AI_KEY: process.env.OPEN_AI_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   async rewrites() {

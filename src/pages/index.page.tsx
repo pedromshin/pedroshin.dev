30ad7850025339e7f160288d71af132df1882e2a
Pedro Shin <pedromaschio.shin@gmail.com>
2023-07-20 18:03:42 -0300

    base embeddings page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -46,6 +46,19 @@ export default function Home() {
           </h2>
         </a>
         <a
+          href="/chatbot"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+            Chatbot with embeddings
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </a>
+        <a
           href="/image"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"

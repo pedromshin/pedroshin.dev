8e4a115e521055e263bdb46669dc5758016645af
Pedro Shin <pedromaschio.shin@gmail.com>
2023-07-27 15:51:15 -0300

    create and save embeddings to supabase

--- scripts/embeddings-notion/embed.ts
+++ scripts/embeddings-notion/embed.ts
@@ -5,7 +5,21 @@ import { createClient } from "@supabase/supabase-js";
 
 loadEnvConfig("");
 
-const generateEmbeddings = async (texts: any) => {
+type Chunk = {
+  content: string;
+  content_length: number;
+  content_tokens: number;
+  embedding: number[];
+};
+
+type ChunkedText = {
+  content: string;
+  length: number;
+  tokens: number;
+  chunks: Chunk[];
+};
+
+const generateEmbeddings = async (texts: ChunkedText[]) => {
   const configuration = new Configuration({
     apiKey: process.env.OPEN_AI_KEY,
   });
@@ -15,6 +29,45 @@ const generateEmbeddings = async (texts: any) => {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
+
+  for (let i = 0; i < texts.length; i++) {
+    const section = texts[i];
+
+    for (let j = 0; j < section.chunks.length; j++) {
+      const chunk = section.chunks[j];
+
+      const { content, content_length, content_tokens } = chunk;
+
+      const embeddingResponse = await openai.createEmbedding({
+        model: "text-embedding-ada-002",
+        input: content,
+      });
+
+      const [{ embedding }] = embeddingResponse.data.data;
+
+      const { data, error } = await supabase
+        .from("notion_embeddings")
+        .insert({
+          content,
+          content_tokens,
+          content_length,
+          embedding,
+        })
+        .select("*");
+
+      if (error) {
+        console.log("error", error);
+      } else {
+        console.log(
+          "sucessfully saved embedding of " +
+            `section ${j + 1}/${section.chunks.length} of`,
+          `chunk ${i + 1}/${texts.length}`
+        );
+      }
+
+      await new Promise((resolve) => setTimeout(resolve, 200));
+    }
+  }
 };
 
 (async () => {

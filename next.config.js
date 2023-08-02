0aabd45a3579fd55a385276309e0e8475fe8bb49
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-01 22:01:17 -0300

    remove unnecessary envs in next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  GITHUB_ID: process.env.GITHUB_ID,
-  GITHUB_SECRET: process.env.GITHUB_SECRET,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,7 @@
+export default function PageContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="">{children}</div>;
+}

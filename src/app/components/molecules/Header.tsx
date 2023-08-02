180eb6263c6b646f63ccc08c96454887b4cc68a2
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-02 01:21:52 -0300

    improves header

--- src/app/components/molecules/Header.tsx
+++ src/app/components/molecules/Header.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Button from "../atoms/Button";
+import { signOut } from "next-auth/react";
 
 export default () => {
   return (
-    <header className="flex flex-row p-4 border-b-2">
+    <header className="flex flex-row p-4 border-b-2 justify-between align-center">
       <h1 className="text-2xl font-bold">
         <Link href={"/home"}>pedroshin.dev</Link>
       </h1>
+      <nav className="flex flex-row align-center">
+        <Link href={"/home"}>Home</Link>
+      </nav>
+      <button onClick={() => signOut()}>Signout</button>
     </header>
   );
 };

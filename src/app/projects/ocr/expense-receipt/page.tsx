e64e9d8bbcc3eabe8020daa16cf187d97325e075
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-07 00:25:41 -0300

    adds table to other ocr pages

--- src/app/projects/ocr/expense-receipt/page.tsx
+++ src/app/projects/ocr/expense-receipt/page.tsx
@@ -2,15 +2,19 @@
 import PageContainer from "@Src/app/components/templates/PageContainer";
 import Heading from "@Src/app/components/organisms/Heading";
 import Dropzone from "@Src/app/components/atoms/Dropzone";
+import OCRResultTable from "@Src/app/components/molecules/OCRResultTable";
 
 export default () => {
   return (
     <PageContainer>
       <Heading title="OCR Expense Receipt">
-        <Dropzone
-          accept="image/png, image/jpeg, application/pdf"
-          onChange={(file) => console.log(file)}
-        />
+        <div className="flex flex-col w-full gap-8">
+          <Dropzone
+            accept="image/png, image/jpeg, application/pdf"
+            onChange={(file) => console.log(file)}
+          />
+          <OCRResultTable data={[]} />
+        </div>
       </Heading>
     </PageContainer>
   );

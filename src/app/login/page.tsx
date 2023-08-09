acbfdc7e8e6df69602e0c84606eedefee1e6f44b
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-09 05:42:18 -0300

    fix deploy

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,11 +13,11 @@ export default () => {
     <main className="h-full w-full flex flex-col items-center justify-center">
       <div className="flex flex-col p-16 border rounded-3xl gap-y-6">
         <h1 className="text-4xl">Login</h1>
-        <Button callback={() => signIn("github")}>
+        <Button onClick={() => signIn("github")}>
           <IconBrandGithub className="mr-2" />
           GitHub
         </Button>
-        <Button callback={() => signIn("google")}>
+        <Button onClick={() => signIn("google")}>
           <IconBrandGoogle className="mr-2" />
           Google
         </Button>

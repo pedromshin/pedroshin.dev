abc9396bcd8a6601f96a126f98a66ecfdc67aa87
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-14 01:34:17 -0300

    fix python nb path

--- app/projects/development/machine-learning/genetic-algorithm/page.tsx
+++ app/projects/development/machine-learning/genetic-algorithm/page.tsx
@@ -6,7 +6,7 @@ export default () => {
       <Heading title="Genetic Algorithm">
         <></>
         <iframe
-          src="https://python.pedroshin.dev/retro/notebooks/?path=notebooks/chromossome.ipynb"
+          src="https://python.pedroshin.dev/retro/notebooks/?path=notebooks/genetic-algorithm.ipynb"
           width="100%"
           height="500px"
           className="max-w-[960px] mx-auto"

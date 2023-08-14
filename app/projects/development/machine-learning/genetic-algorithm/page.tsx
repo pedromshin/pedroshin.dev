9a182be8393bb5861b5087c8ec7ae0b3ed477ffa
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-14 02:05:16 -0300

    update python notebook embed url

--- app/projects/development/machine-learning/genetic-algorithm/page.tsx
+++ app/projects/development/machine-learning/genetic-algorithm/page.tsx
@@ -6,7 +6,7 @@ export default () => {
       <Heading title="Genetic Algorithm">
         <></>
         <iframe
-          src="https://python.pedroshin.dev/retro/notebooks/?path=notebooks/genetic-algorithm.ipynb"
+          src="https://python.pedroshin.dev/retro/notebooks/?path=notebooks/genetic-algorithm/genetic-algorithm.ipynb"
           width="100%"
           height="500px"
           className="max-w-[960px] mx-auto"

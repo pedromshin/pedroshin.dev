06cb15e14b85fb4d0cdbe6bddf893f6e59271c76
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-14 02:29:10 -0300

    improve ipynb height

--- app/projects/development/machine-learning/genetic-algorithm/page.tsx
+++ app/projects/development/machine-learning/genetic-algorithm/page.tsx
@@ -1,14 +1,22 @@
+"use client";
+import { useState, useRef, useEffect } from "react";
 import Heading from "@App/components/organisms/Heading";
 
 export default () => {
+  const [height, setHeight] = useState<string>();
+
+  useEffect(() => {
+    !height && setHeight(window.innerHeight - 100 + "px");
+  }, []);
+
   return (
     <>
       <Heading title="Genetic Algorithm">
         <></>
         <iframe
+          height={height}
           src="https://python.pedroshin.dev/retro/notebooks/?path=notebooks/genetic-algorithm/genetic-algorithm.ipynb"
           width="100%"
-          height="500px"
           className="max-w-[960px] mx-auto"
         ></iframe>
       </Heading>

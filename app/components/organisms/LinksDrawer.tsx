eb44f63a450c3287c6b748b26fe47bb5d95bc7eb
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-17 01:22:18 -0300

    fix size drawer

--- app/components/organisms/LinksDrawer.tsx
+++ app/components/organisms/LinksDrawer.tsx
@@ -10,8 +10,7 @@ export default ({ onClose, open }: { onClose: () => void; open: boolean }) => {
     <Drawer
       open={open}
       onClose={onClose}
-      size={0.6 * window.innerWidth}
-      className="p-4 bg-black bg-opacity-50 border-l-2 border-white-700/50"
+      className="p-4 bg-black bg-opacity-60 border-l-2 border-white-700/50"
       placement="right"
     >
       <div className="mb-6 h-full flex flex-col items-center justify-between">

7b706917bafc77f5ab24399aa48600699b239983
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-23 23:16:35 -0300

    adds js frameworks links

--- app/links.ts
+++ app/links.ts
@@ -29,6 +29,50 @@ export const links: LinkType[] = [
     ],
   },
   {
+    title: "Chatbot",
+    description: "Open chatbot",
+    slug: "/chatbot",
+    subitems: [
+      {
+        title: "Open",
+        slug: "/open",
+        description: "Default chatbot with gpt-3.5-turbo",
+      },
+      {
+        title: "Flash chatbot (word embeddings)",
+        slug: "/flash",
+        description: "Chatbot trained with content from a Notion page",
+      },
+    ],
+  },
+  {
+    title: "Frontend frameworks",
+    description: "Subdomains deployed in various frontend frameworks",
+    slug: "/",
+    subitems: [
+      {
+        title: "Angular",
+        slug: "https://angular.pedroshin.dev",
+        external: true,
+      },
+      {
+        title: "Solid",
+        slug: "https://solid.pedroshin.dev",
+        external: true,
+      },
+      {
+        title: "Svelte",
+        slug: "https://svelte.pedroshin.dev",
+        external: true,
+      },
+      {
+        title: "Vue",
+        slug: "https://vue.pedroshin.dev",
+        external: true,
+      },
+    ],
+  },
+  {
     title: "OCR",
     description: "",
     slug: "/ocr",
@@ -51,48 +95,31 @@ export const links: LinkType[] = [
     ],
   },
   {
-    title: "Audio",
-    description: "Extract subtitle from video and transcribe audio",
-    slug: "/audio",
-    subitems: [
-      {
-        title: "Subtitle generation",
-        slug: "/subtitle",
-        description: "Generate subtitles from a video",
-      },
-      {
-        title: "Audio transcription",
-        slug: "/transcription",
-        description: "Transcribe audio from a voice recording",
-      },
-    ],
-  },
-  {
-    title: "Image",
-    slug: "/image",
-    description: "Generate images by a text prompt (R$0,20 per image)",
-  },
-  {
-    title: "Chatbot",
-    description: "Open chatbot",
-    slug: "/chatbot",
-    subitems: [
-      {
-        title: "Open",
-        slug: "/open",
-        description: "Default chatbot with gpt-3.5-turbo",
-      },
-      {
-        title: "Flash chatbot (word embeddings)",
-        slug: "/flash",
-        description: "Chatbot trained with content from a Notion page",
-      },
-    ],
-  },
-  {
     title: "Em desenvolvimento / criação",
     slug: "/development",
     subitems: [
+      {
+        title: "Audio",
+        description: "Extract subtitle from video and transcribe audio",
+        slug: "/audio",
+        subitems: [
+          {
+            title: "Subtitle generation",
+            slug: "/subtitle",
+            description: "Generate subtitles from a video",
+          },
+          {
+            title: "Audio transcription",
+            slug: "/transcription",
+            description: "Transcribe audio from a voice recording",
+          },
+        ],
+      },
+      {
+        title: "Image",
+        slug: "/image",
+        description: "Generate images by a text prompt (R$0,20 per image)",
+      },
       {
         title: "Resumé data extraction",
         slug: "/resume-extraction",

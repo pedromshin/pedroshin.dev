16f784ad04a5c4355d71fd11ef6126bb7298c2a0
Pedro Shin <pedromaschio.shin@gmail.com>
2023-08-27 20:33:24 -0300

    fix supabase console warn

--- app/clients/supabase-client.ts
+++ app/clients/supabase-client.ts
@@ -3,5 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 
 export default createClient(
   envs.NEXT_PUBLIC_SUPABASE_URL,
-  envs.SUPABASE_SERVICE_ROLE_KEY
+  envs.SUPABASE_SERVICE_ROLE_KEY,
+  {
+    auth: { persistSession: false },
+  }
 );

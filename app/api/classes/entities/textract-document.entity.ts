11ac4e41832c808898071a1510a4210e405a1c91
Pedro Shin <pedromaschio.shin@gmail.com>
2023-09-06 18:51:12 -0300

    improve ocr

--- app/api/classes/entities/textract-document.entity.ts
+++ app/api/classes/entities/textract-document.entity.ts
@@ -29,49 +29,49 @@ export default class TextractableDocumentEntity {
     return commonErrors.includes(resultValue) ? "" : resultValue;
   }
 
-  private findLine(resultBlock: Block) {
-    const line = this.blocks.find((block) => {
-      const blockTop = block.Geometry?.BoundingBox?.Top?.toFixed(3);
-      const resultBlockTop =
-        resultBlock?.Geometry?.BoundingBox?.Top?.toFixed(3);
+  // private findLine(resultBlock: Block) {
+  //   const line = this.blocks.find((block) => {
+  //     const blockTop = block.Geometry?.BoundingBox?.Top?.toFixed(3);
+  //     const resultBlockTop =
+  //       resultBlock?.Geometry?.BoundingBox?.Top?.toFixed(3);
 
-      return (
-        block.BlockType === "LINE" &&
-        Math.abs(+blockTop! - +resultBlockTop!) < 0.02
-      );
-    })?.Text;
+  //     return (
+  //       block.BlockType === "LINE" &&
+  //       Math.abs(+blockTop! - +resultBlockTop!) < 0.05
+  //     );
+  //   })?.Text;
 
-    return line ?? "";
-  }
+  //   return line ?? "";
+  // }
 
-  private normalizeSpecialCharacters(resultValue: string) {
-    let normalizedResultValue = resultValue;
-    for (const resultBlock of this.resultBlocks) {
-      const resultLineText = this.findLine(resultBlock);
+  // private normalizeSpecialCharacters(resultValue: string) {
+  //   let normalizedResultValue = resultValue;
+  //   for (const resultBlock of this.resultBlocks) {
+  //     const resultLineText = this.findLine(resultBlock);
 
-      const normalizedResultLineText = resultLineText?.replace(
-        this.specialCharsRegex,
-        ""
-      );
+  //     const normalizedResultLineText = resultLineText?.replace(
+  //       this.specialCharsRegex,
+  //       ""
+  //     );
 
-      if (
-        normalizedResultLineText?.includes(resultValue) ||
-        resultValue?.includes(normalizedResultLineText!)
-      ) {
-        const normalizedWords = resultLineText.split(" ");
-        const queryResultWords: string[] = resultValue?.split(" ");
-        const indexes: number[] = [];
-        normalizedWords.forEach((word, index) => {
-          if (word === queryResultWords[index]) indexes.push(index);
-        });
-        indexes.forEach(
-          (index) => (queryResultWords[index] = normalizedWords[index])
-        );
-        normalizedResultValue = queryResultWords.join(" ");
-      }
-    }
-    return resultValue;
-  }
+  //     if (
+  //       normalizedResultLineText?.includes(resultValue) ||
+  //       resultValue?.includes(normalizedResultLineText!)
+  //     ) {
+  //       const normalizedWords = resultLineText.split(" ");
+  //       const queryResultWords: string[] = resultValue?.split(" ");
+  //       const indexes: number[] = [];
+  //       normalizedWords.forEach((word, index) => {
+  //         if (word === queryResultWords[index]) indexes.push(index);
+  //       });
+  //       indexes.forEach(
+  //         (index) => (queryResultWords[index] = normalizedWords[index])
+  //       );
+  //       normalizedResultValue = queryResultWords.join(" ");
+  //     }
+  //   }
+  //   return resultValue;
+  // }
 
   private validateConfidence(
     resultConfidence: number,
@@ -79,7 +79,8 @@ export default class TextractableDocumentEntity {
     resultValue: string
   ): void {
     const validationEntry =
-      resultConfidence < 30
+      // Disabled confidence validation
+      resultConfidence < 0
         ? { field: blockAlias, error: "ERROR_LOW_CONFIDENCE" }
         : { field: blockAlias, value: resultValue };
 
@@ -103,13 +104,13 @@ export default class TextractableDocumentEntity {
       }
 
       const resultValue = this.detectErrors(resultBlock?.Text ?? "");
-      const normalizedResultValue =
-        this.normalizeSpecialCharacters(resultValue);
+      // const normalizedResultValue =
+      //   this.normalizeSpecialCharacters(resultValue);
 
       this.validateConfidence(
         resultBlock?.Confidence ?? 0,
         queryBlock.Query?.Alias!,
-        normalizedResultValue
+        resultValue
       );
     }
 
